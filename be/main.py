6f676346746ceedb15264bfaa3012d54a13ca3d8
Nguyen Gia Huu Phuoc <phuocnghse184632@fpt.edu.vn>
2026-06-22 08:44:01 +0700

    a

--- be/main.py
+++ be/main.py
@@ -22,7 +22,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
